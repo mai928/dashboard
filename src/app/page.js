92b3db6a520000c79fa9e9a8055201e1e8bbef14
maimohammed928 <maimohammed928@gmail.com>
2025-07-25 17:19:46 +0300

    splash screen

--- src/app/page.js
+++ src/app/page.js
@@ -4,16 +4,16 @@ import "./globals.css";
 export default function Home() {
 	return (
 		<main className="min-h-screen flex flex-col justify-center items-center bg-gradient-to-b from-amber-50 to-white">
-			<div className=" bg-root px-28 py-32 rounded-3xl shadow-lg">
+			<div className=" bg-root mx-5 px-5 lg:px-28 py-10 lg:py-32 rounded-3xl shadow-lg">
         <div className="text-center text-black">
-        			<h1 className="text-3xl font-bold ">Welcome to the Project</h1>
+        			<h1 className=" text-xl lg:text-3xl font-bold ">Welcome to the Project</h1>
 				<p className="text-gray-700 text-sm mt-2">Choose where you want to go</p>  
         </div>
 	
 
   			<div className=" my-10">
 					<Link  href="/AdminDashboard">
-						<button className=" flex m-auto bg-gradient-to-t from-blue-700 to-[#2651f8] text-white text-lg px-10 py-3 rounded-xl capitalize font-semibold">
+						<button className=" flex m-auto bg-gradient-to-t from-blue-700 to-[#2651f8] text-white lg:text-lg px-10 py-3 rounded-xl capitalize font-semibold">
 							Go to Dashboard
 						</button>
 					</Link>
@@ -21,7 +21,7 @@ export default function Home() {
 
 				<div>
 					<Link href="/Store">
-						<button className=" font-pacifico flex m-auto bg-gradient-to-r from-[#ffcf64] to-[#ff8969] text-black text-3xl font-semibold px-14 py-3 rounded-full">
+						<button className=" font-pacifico flex m-auto bg-gradient-to-r from-[#ffcf64] to-[#ff8969] text-black text-xl lg:text-3xl font-semibold px-14 py-3 rounded-full">
 							Go to Store
 						</button>
 					</Link>
